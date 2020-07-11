847826d66ec6c818ee5334a1998054965e38e885
Hiba Jamal <hj04033@st.habib.edu.pk>
2020-07-11 23:44:58 +0500

    added dataset extract ipynb

--- GEOTiff_Manipulation/gdal_test.py
+++
@@ -1,9 +0,0 @@
-from osgeo import gdal
-import matplotlib.pyplot as plt
-
-ds = gdal.Open('try2.tif').ReadAsArray()
-ds = ds.reshape((ds.shape[1], ds.shape[2], 3))
-print(ds.shape)
-
-plt.imshow(ds)
-plt.show()
